758764f99bdcafbe1d0d9b1e6b70b5da1b127942
HuangJyunYu <yillkid@gmail.com>
2025-01-30 17:14:10 +0900

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,4 @@
 // Host
-// HOST_URL_EID_DAEMON = "https://beta-eid-backend.4impact.cc";
-// HOST_URL_TPLANET_DAEMON = "https://beta-tplanet-backend.4impact.cc";
-
 HOST_URL_EID_DAEMON = "https://rc-eid-backend.4impact.cc";
 HOST_URL_TPLANET_DAEMON = "https://rc-tplanet-backend.4impact.cc";
 
